9ec56c1e6fc260de9e6e0848145efeea884408b9
Tim Jespers <tjespers@personeelssysteem.nl>
2017-08-24 12:44:43 +0200

    changed column offset from 2 to 1

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -73,7 +73,7 @@
 
         <div class="container">
             <div class="row">
-                <div class="col-sm-10 col-sm-offset-2">
+                <div class="col-sm-10 col-sm-offset-1">
                     @yield('content')
                 </div>
             </div>
