42d1888f75f2dddd10bd28987218e9ee1601f3f6
Clara van Miert <c.vanmiert@global.leaseweb.com>
2017-08-24 10:53:45 +0200

    lists challenges on /challenges page

--- app/Http/Controllers/ChallengeController.php
+++ app/Http/Controllers/ChallengeController.php
@@ -4,15 +4,14 @@ declare(strict_types = 1);
 namespace App\Http\Controllers;
 
 use App\Models\Challenge;
-use Illuminate\Http\Request;
+use Illuminate\View\View;
 
 class ChallengeController extends Controller
 {
-    public function listAll()
+    public function listAll(): View
     {
-
         $challenges = Challenge::all();
-        dd($challenges);
+
         return view('challenges.list', compact('challenges'));
     }
 }
