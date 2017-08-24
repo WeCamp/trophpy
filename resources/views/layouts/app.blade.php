91acd436055eb20c5406aaa92c66df9ad53ca9d4
Tim Jespers <tjespers@personeelssysteem.nl>
2017-08-24 12:42:09 +0200

    Wrapped content in a container, row, columns HTML structure

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -71,7 +71,13 @@
             </div>
         </nav>
 
-        @yield('content')
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-10 col-sm-offset-2">
+                    @yield('content')
+                </div>
+            </div>
+        </div>
     </div>
 
     <!-- Scripts -->
