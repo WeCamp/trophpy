375a85c1d85a82caf35105d92ddcbfa8cb152d45
jaspermutsaerts@gmail.com <=>
2017-08-24 18:27:47 +0200

    whitespace removed

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Challenge;
 use App\Models\User;
 use Illuminate\View\View;
 
@@ -10,12 +9,10 @@ class UsersController extends Controller
 {
     public function view(User $user): View
     {
-
-
         return view('users.view')->with([
             'user' => $user->toArray(),
             'currentChallenges' => $user->currentChallenges->toArray(),
-            'completedChallenges' =>  $user->completedChallenges->toArray(),
+            'completedChallenges' => $user->completedChallenges->toArray(),
         ]);
     }
 }
