6471bb2541bef7dcb7fcc1bf5b1b23e5ea414121
jaspermutsaerts@gmail.com <=>
2017-08-25 18:55:42 +0200

    fake some started_on dates in seeder

--- database/seeds/UserChallengesTableSeeder.php
+++ database/seeds/UserChallengesTableSeeder.php
@@ -12,12 +12,12 @@ class UserChallengesTableSeeder extends Seeder
     public function run(\Faker\Generator $faker)
     {
         DB::table('user_challenges')->insert([
-            ['user_id' => 1, 'challenge_id' => 1, 'created_at' => $faker->dateTime(), 'updated_at' => $faker->dateTime()],
-            ['user_id' => 1, 'challenge_id' => 2, 'created_at' => $faker->dateTime(), 'updated_at' => $faker->dateTime()],
-            ['user_id' => 1, 'challenge_id' => 3, 'created_at' => $faker->dateTime(), 'updated_at' => $faker->dateTime()],
-            ['user_id' => 2, 'challenge_id' => 4, 'created_at' => $faker->dateTime(), 'updated_at' => $faker->dateTime()],
-            ['user_id' => 2, 'challenge_id' => 5, 'created_at' => $faker->dateTime(), 'updated_at' => $faker->dateTime()],
-            ['user_id' => 2, 'challenge_id' => 6, 'created_at' => $faker->dateTime(), 'updated_at' => $faker->dateTime()],
+            ['user_id' => 1, 'challenge_id' => 1, 'started_on' => $faker->dateTime(), 'created_at' => $faker->dateTime(), 'updated_at' => $faker->dateTime()],
+            ['user_id' => 1, 'challenge_id' => 2, 'started_on' => $faker->dateTime(), 'created_at' => $faker->dateTime(), 'updated_at' => $faker->dateTime()],
+            ['user_id' => 1, 'challenge_id' => 3, 'started_on' => $faker->dateTime(), 'created_at' => $faker->dateTime(), 'updated_at' => $faker->dateTime()],
+            ['user_id' => 2, 'challenge_id' => 4, 'started_on' => $faker->dateTime(), 'created_at' => $faker->dateTime(), 'updated_at' => $faker->dateTime()],
+            ['user_id' => 2, 'challenge_id' => 5, 'started_on' => $faker->dateTime(), 'created_at' => $faker->dateTime(), 'updated_at' => $faker->dateTime()],
+            ['user_id' => 2, 'challenge_id' => 6, 'started_on' => $faker->dateTime(), 'created_at' => $faker->dateTime(), 'updated_at' => $faker->dateTime()],
          ]);
     }
 }
