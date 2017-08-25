b5a858d7c4014a6558fd6e03383f8bedef8f90d9
jaspermutsaerts@gmail.com <=>
2017-08-25 09:50:57 +0200

    Add challenges for other users to show it is filtered

--- database/seeds/UserChallengesTableSeeder.php
+++ database/seeds/UserChallengesTableSeeder.php
@@ -15,6 +15,9 @@ class UserChallengesTableSeeder extends Seeder
             ['user_id' => 1, 'challenge_id' => 1, 'created_at' => $faker->dateTime(), 'updated_at' => $faker->dateTime()],
             ['user_id' => 1, 'challenge_id' => 2, 'created_at' => $faker->dateTime(), 'updated_at' => $faker->dateTime()],
             ['user_id' => 1, 'challenge_id' => 3, 'created_at' => $faker->dateTime(), 'updated_at' => $faker->dateTime()],
+            ['user_id' => 2, 'challenge_id' => 4, 'created_at' => $faker->dateTime(), 'updated_at' => $faker->dateTime()],
+            ['user_id' => 2, 'challenge_id' => 5, 'created_at' => $faker->dateTime(), 'updated_at' => $faker->dateTime()],
+            ['user_id' => 2, 'challenge_id' => 6, 'created_at' => $faker->dateTime(), 'updated_at' => $faker->dateTime()],
          ]);
     }
 }
